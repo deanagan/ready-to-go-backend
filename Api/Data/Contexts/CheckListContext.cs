cfc62959d0fb2aade7a2e93d22cf2827a4bdc296
Dean Agan <agandfr@gmail.com>
2020-06-19 20:26:52 +1000

    Add checklist context with mapping one to one for item and item detail

---
+++ Api/Data/Contexts/CheckListContext.cs
@@ -0,0 +1,83 @@
+using Microsoft.EntityFrameworkCore;
+using Api.Data.Models;
+
+namespace Api.Data.Contexts
+{
+    public class CarClubContext : DbContext
+    {
+        public CarClubContext(DbContextOptions<CarClubContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items {get;set;}
+        public DbSet<ItemDetail> ItemDetails {get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<ItemDetail>()
+                .HasOne<Item>(i => i.Item)
+                .WithOne(itd => itd.ItemDetail)
+                .HasForeignKey<Item>(itd => itd.Id);
+
+            modelBuilder.Entity<Item>(
+                i => {
+                    i.HasKey(i => i.Id);
+                }
+            );
+
+            modelBuilder.Entity<Item>().HasData(
+                new Item {
+                    Id = 1,
+                    Name = "T-shirt",
+                    ItemDetailId = 1
+                },
+                new Item {
+                    Id = 2,
+                    Name = "Pants",
+                    ItemDetailId = 2
+                },
+                new Item {
+                    Id = 3,
+                    Name = "Water Bottle",
+                    ItemDetailId = 3
+                },
+                new Item {
+                    Id = 4,
+                    Name = "Toothbrush",
+                    ItemDetailId = 4
+                }
+            );
+
+            modelBuilder.Entity<ItemDetail>().HasData(
+                new ItemDetail {
+                    Id = 1,
+                    Description = "Extra T-shirts",
+                    Ready = false,
+                    Quantity = 3
+                },
+                new ItemDetail {
+                    Id = 1,
+                    Description = "Extra Pants",
+                    Ready = false,
+                    Quantity = 1
+                },
+                new ItemDetail {
+                    Id = 1,
+                    Description = "Empty Bottle",
+                    Ready = false,
+                    Quantity = 1
+                },
+                new ItemDetail {
+                    Id = 1,
+                    Description = "Bring the electric one!",
+                    Ready = false,
+                    Quantity = 1
+                }
+            );
+        }
+
+    }
+}

3354df44f95c1f7ab330de58b9842fe1aa7a0c3a
Dean Agan <agandfr@gmail.com>
2020-06-19 19:48:02 +1000

    Implement basic controller

---
+++ Api/Controllers/CheckListController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CheckListController : ControllerBase
+    {
+
+        private readonly ILogger<CheckListController> _logger;
+
+        public CheckListController(ILogger<CheckListController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetAllLists()
+        {
+            return Ok();
+        }
+
+        [HttpGet("[action]/{id}")]
+        public IActionResult GetList(int id)
+        {
+            return Ok();
+        }
+    }
+}

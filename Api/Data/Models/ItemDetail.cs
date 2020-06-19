17e906a38aa2ee9bfddddbc04ea3494e1e78643b
Dean Agan <agandfr@gmail.com>
2020-06-19 20:25:56 +1000

    Add quantity

--- Api/Data/Models/ItemDetail.cs
+++ Api/Data/Models/ItemDetail.cs
@@ -6,7 +6,7 @@ namespace Api.Data.Models
         public int Id { get; set; }
         public string Description { get; set; }
         public bool Ready { get; set; }
-
+        public int Quantity { get; set; }
         public Item Item { get; set; }
     }
 }

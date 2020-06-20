265d3354791c443bf167262089432853ab3059e0
Dean Agan <agandfr@gmail.com>
2020-06-20 12:27:56 +1000

    Store migration files

---
+++ Api/Migrations/20200619111725_InitialCreate.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheckLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ItemDetailId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CheckListToItems",
+                columns: table => new
+                {
+                    CheckListId = table.Column<int>(nullable: false),
+                    ItemId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckListToItems", x => new { x.ItemId, x.CheckListId });
+                    table.ForeignKey(
+                        name: "FK_CheckListToItems_CheckLists_CheckListId",
+                        column: x => x.CheckListId,
+                        principalTable: "CheckLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CheckListToItems_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Ready = table.Column<bool>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemDetails_Items_Id",
+                        column: x => x.Id,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CheckLists",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "This is a checklist for my full marathon!", "T-shirt" });
+
+            migrationBuilder.InsertData(
+                table: "Items",
+                columns: new[] { "Id", "ItemDetailId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "T-shirt" },
+                    { 2, 2, "Pants" },
+                    { 3, 3, "Water Bottle" },
+                    { 4, 4, "Toothbrush" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CheckListToItems",
+                columns: new[] { "ItemId", "CheckListId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 1 },
+                    { 3, 1 },
+                    { 4, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ItemDetails",
+                columns: new[] { "Id", "Description", "Quantity", "Ready" },
+                values: new object[,]
+                {
+                    { 1, "Extra T-shirts", 3, false },
+                    { 2, "Extra Pants", 1, false },
+                    { 3, "Empty Bottle", 1, false },
+                    { 4, "Bring the electric one!", 1, false }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheckListToItems_CheckListId",
+                table: "CheckListToItems",
+                column: "CheckListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckListToItems");
+
+            migrationBuilder.DropTable(
+                name: "ItemDetails");
+
+            migrationBuilder.DropTable(
+                name: "CheckLists");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
---
+++ Api/Migrations/20200620021650_ModifiedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class ModifiedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "CheckLists",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Full Marathon");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "CheckLists",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "T-shirt");
+        }
+    }
+}

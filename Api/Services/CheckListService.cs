8889003377494b3e22ba39d57eb5124c41786d29
Dean Agan <agandfr@gmail.com>
2020-06-29 20:28:56 +1000

    Adding in get method for check list service

--- Api/Services/CheckListService.cs
+++ Api/Services/CheckListService.cs
@@ -14,121 +14,57 @@ namespace Api.Services
         {
             _unitOfWork = unitOfWork;
         }
+
         public CheckListView GetCheckList(int id)
         {
-            var checkListWithItemDetails = _unitOfWork.CheckLists.GetAll()
-                            .Join(
-                                _unitOfWork.CheckListToItemDetails.GetAll(),
-                                checklist => checklist.Id,
-                                checklistToItem => checklistToItem.CheckListId,
-                                (checklist, checklistToItem) => new
-                                {
-                                    checkListId = checklist.Id,
-                                    checkListItemDetailId = checklistToItem.ItemDetailId,
-                                    checkListName = checklist.Name,
-                                    checklistDescription = checklist.Description
-                                }
-                            )
-                            .Where(
-                                queryResult => queryResult.checkListId == id
-                            );
-
-            var items = checkListWithItemDetails
-                        .Join(
-                            _unitOfWork.Items.GetAll(),
-                            checklistWithItemId => checklistWithItemId.checkListItemDetailId,
-                            items => items.Id,
-                            (checkListWithItemIds, items) => new
-                            {
-                                itemId = items.Id,
-                                itemDetailId = checkListWithItemIds.checkListItemDetailId,
-                                itemDescription = items.Description,
-                                itemName = items.Name
-                            }
-                        );
-
-            var itemDetails = items
-                              .Join(
-                                _unitOfWork.ItemDetails.GetAll(),
-                                item => item.itemDetailId,
-                                itemDetail => itemDetail.Id,
-                                (item, itemDetail) => new
-                                {
-                                    itemName = item.itemName,
-                                    itemDescription = item.itemDescription,
-                                    itemReadyState = itemDetail.Ready,
-                                    itemQuantity = itemDetail.Quantity
-                                }
-                              );
             var checkList =  _unitOfWork.CheckLists.Get(id);
-
 
             return new CheckListView {
                     Name = checkList.Name,
                     Description = checkList.Description,
-                    Items = (from Item in itemDetails
-                            select new ItemView
-                            {
-                                Name = Item.itemName,
-                                Description = Item.itemDescription,
-                                IsReady = Item.itemReadyState,
-                                Quantity = Item.itemQuantity
-                            }).ToList()
+                    Items = GetItemViews(id).ToList()
                 };
         }
 
         public IEnumerable<CheckListView> GetCheckLists()
         {
-            return new List<CheckListView> {
-                new CheckListView {
-                    Name = "Marathon",
-                    Description = "A checklist on what I need to do tomorrow",
-                    Items =  new List<ItemView> {
-                        new ItemView {
-                            Name = "Water Bottle",
-                            Description = "Something to drink from",
-                            IsReady = true,
-                            Quantity = 1
-                        },
-                        new ItemView {
-                            Name = "Singlet",
-                            Description = "This is required for the race",
-                            IsReady = true,
-                            Quantity = 1
-                        },
-                        new ItemView {
-                            Name = "Fitbit",
-                            Description = "Don't forget to bring this so we have a tracker.",
-                            IsReady = true,
-                            Quantity = 1
-                        }
+            var items =
+                   (from checkLists in _unitOfWork.CheckLists.GetAll()
+                    select new CheckListView {
+                        Name = checkLists.Name,
+                        Description = checkLists.Description,
+                        Items = (from checklistItemDetail in _unitOfWork.CheckListToItemDetails.GetAll()
+                            where checklistItemDetail.CheckListId == checkLists.Id
+                            join itemDetail in _unitOfWork.ItemDetails.GetAll()
+                            on checklistItemDetail.ItemDetailId equals itemDetail.Id
+                            join item in _unitOfWork.Items.GetAll()
+                            on itemDetail.ItemId equals item.Id
+                            select new ItemView {
+                                Name = item.Name,
+                                Description = item.Description,
+                                Quantity = itemDetail.Quantity,
+                                IsReady = itemDetail.Ready,
+                            }).ToList()
                     }
-                },
-                new CheckListView {
-                    Name = "Basketball Game",
-                    Description = "A checklist for when I play basketball",
-                    Items =  new List<ItemView> {
-                        new ItemView {
-                            Name = "Water Bottle",
-                            Description = "Something to drink from",
-                            IsReady = true,
-                            Quantity = 1
-                        },
-                        new ItemView {
-                            Name = "Mouth guard",
-                            Description = "Need this to avoid breaking my teeth",
-                            IsReady = true,
-                            Quantity = 1
-                        },
-                        new ItemView {
-                            Name = "Headband",
-                            Description = "Bring black head band.",
-                            IsReady = true,
-                            Quantity = 1
-                        }
-                    }
-                },
-            };
+                    );
+            return items.ToList();
+        }
+
+        private IEnumerable<ItemView> GetItemViews(int id)
+        {
+            var itemViews = from checklistItemDetail in _unitOfWork.CheckListToItemDetails.GetAll()
+                            where checklistItemDetail.CheckListId == id
+                            join itemDetail in _unitOfWork.ItemDetails.GetAll()
+                            on checklistItemDetail.ItemDetailId equals itemDetail.Id
+                            join item in _unitOfWork.Items.GetAll()
+                            on itemDetail.ItemId equals item.Id
+                            select new ItemView {
+                                Name = item.Name,
+                                Description = item.Description,
+                                Quantity = itemDetail.Quantity,
+                                IsReady = itemDetail.Ready,
+                            };
+            return itemViews.AsEnumerable();
         }
     }
 }

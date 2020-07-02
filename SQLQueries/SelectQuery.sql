e567d028ec42a4fc9d0d71472493e152b374314f
Dean Agan <agandfr@gmail.com>
2020-07-02 18:38:55 +1000

    Updating sample DB query to include notes

--- SQLQueries/SelectQuery.sql
+++ SQLQueries/SelectQuery.sql
@@ -9,6 +9,7 @@ SELECT CheckLists.Name
         , CheckLists.Description
         , Items.Name
         , Items.Description
+        , ItemDetails.Notes
         , CASE WHEN ItemDetails.Ready = 0 THEN 'Not Ready' ELSE 'Ready' END AS Ready
         , ItemDetails.Quantity
 

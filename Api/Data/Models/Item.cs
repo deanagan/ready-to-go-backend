0a80ec6addfaecd5ac414cfd7405450d43204494
Dean Agan <agandfr@gmail.com>
2020-07-05 14:04:43 +1000

    Updated models

--- Api/Data/Models/Item.cs
+++ Api/Data/Models/Item.cs
@@ -5,12 +5,8 @@ namespace Api.Data.Models
 {
     public class Item
     {
-        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
-        // Navigational Property
-        public ItemDetail ItemDetail { get; set; }
     }
 }
--- Api/Data/Models/ItemDetail.cs
+++ Api/Data/Models/ItemDetail.cs
@@ -1,15 +1,17 @@
-using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace Api.Data.Models
 {
     public class ItemDetail
     {
-        [Key]
         public int Id { get; set; }
         public bool Ready { get; set; }
         public int Quantity { get; set; }
         public string Notes { get; set; }
+
         public int ItemId { get; set; }
         public Item Item { get; set; }
+
+        public ICollection<CheckListToItemDetail> CheckListToItems { get; set; }
     }
 }

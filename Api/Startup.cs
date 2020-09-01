e20707086ea1f0cd48dd92a39be2d129705d83db
Dean Agan <agandfr@gmail.com>
2020-09-01 19:36:07 +1000

    Temporarily disable authorization

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -57,7 +57,7 @@ namespace Api
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {

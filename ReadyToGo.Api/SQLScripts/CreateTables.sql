6c3443b9ea23b28073c97f85b90e0691b783f143
Dean Agan <agandfr@gmail.com>
2021-05-23 18:31:23 +1000

    Adding create table script

--- ReadyToGo.Api/SQLScripts/CreateTables.sql
+++ ReadyToGo.Api/SQLScripts/CreateTables.sql
@@ -2,6 +2,7 @@ CREATE TABLE dbo.Trip(
   TripId int IDENTITY(1,1) NOT NULL,
   Title nvarchar(100) NOT NULL,
   Summary nvarchar(max) NOT NULL,
+  Poster nvarchar(200) NOT NULL,
   DateCreated datetime2(7) NOT NULL,
  CONSTRAINT PK_Trip PRIMARY KEY CLUSTERED
 (
@@ -10,8 +11,21 @@ CREATE TABLE dbo.Trip(
 )
 GO
 
+CREATE TABLE dbo.Image(
+  ImageId int IDENTITY(1,1) NOT NULL,
+  TripId int NOT NULL,
+  Title nvarchar(100) NOT NULL,
+  ImgLocation nvarchar(max) NOT NULL,
+ CONSTRAINT PK_Image PRIMARY KEY CLUSTERED
+(
+  ImageId ASC
+)
+)
+GO
+
 CREATE TABLE dbo.Question(
   QuestionId int IDENTITY(1,1) NOT NULL,
+  TripId int NOT NULL,
   Title nvarchar(100) NOT NULL,
   Content nvarchar(max) NOT NULL,
   UserId nvarchar(150) NOT NULL,
@@ -37,6 +51,15 @@ CREATE TABLE dbo.Answer(
 )
 )
 GO
+
+ALTER TABLE dbo.Question  WITH CHECK ADD  CONSTRAINT FK_Question_Trip FOREIGN KEY(TripId)
+REFERENCES dbo.Trip (TripId)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE dbo.Question CHECK CONSTRAINT FK_Question_Trip
+GO
+
 ALTER TABLE dbo.Answer  WITH CHECK ADD  CONSTRAINT FK_Answer_Question FOREIGN KEY(QuestionId)
 REFERENCES dbo.Question (QuestionId)
 ON UPDATE CASCADE
@@ -45,17 +68,44 @@ GO
 ALTER TABLE dbo.Answer CHECK CONSTRAINT FK_Answer_Question
 GO
 
+ALTER TABLE dbo.Image  WITH CHECK ADD  CONSTRAINT FK_Trip_Image FOREIGN KEY(TripId)
+REFERENCES dbo.Trip (TripId)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE dbo.Image CHECK CONSTRAINT FK_Trip_Image
+GO
+
+SET IDENTITY_INSERT dbo.Trip ON
+GO
+INSERT INTO dbo.Trip(TripId, Title, Summary, Poster, DateCreated)
+VALUES(1,
+    'Bohol Beach Club',
+    'Beach Resort in the Philippines',
+    'joe.test@test.com',
+    '2021-05-18 14:32')
+
+INSERT INTO dbo.Trip(TripId, Title, Summary, Poster, DateCreated)
+VALUES(2,
+    'Imperial Palace, Tokyo, Japan',
+    'Sights and Sounds Japan',
+    'jane.test@test.com',
+    '2021-01-17 11:41')
+GO
+SET IDENTITY_INSERT dbo.Trip OFF
+GO
+
 SET IDENTITY_INSERT dbo.Question ON
 GO
-INSERT INTO dbo.Question(QuestionId, Title, Content, UserId, UserName, Created)
-VALUES(1, 'Do they have room service?',
+INSERT INTO dbo.Question(QuestionId, TripId, Title, Content, UserId, UserName, Created)
+VALUES(1, 1, 'Do they have room service?',
     'It looks like a nice place, but I wonder if they have room service?',
     '1',
     'bob.test@test.com',
     '2021-05-18 14:32')
 
-INSERT INTO dbo.Question(QuestionId, Title, Content, UserId, UserName, Created)
-VALUES(2, 'Which type of card should I use?',
+INSERT INTO dbo.Question(QuestionId, TripId, Title, Content, UserId, UserName, Created)
+VALUES(2, 2, 'Which type of card should I use?',
     'There seems to be a lot of different types of trip cards I can use in Japan. Should I use Suica or Pasmo?',
     '2',
     'jane.test@test.com',

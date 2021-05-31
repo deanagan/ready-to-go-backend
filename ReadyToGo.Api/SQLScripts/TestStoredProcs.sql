dc604f57d5cd448a81b6541e9d530670955120d9
Dean Agan <agandfr@gmail.com>
2021-05-31 20:44:45 +1000

    Adding test code

--- ReadyToGo.Api/SQLScripts/TestStoredProcs.sql
+++ ReadyToGo.Api/SQLScripts/TestStoredProcs.sql
@@ -5,3 +5,9 @@ EXEC dbo.Reporter_Post  @Email="r2d2@readytogo.com",
 
 
 EXEC dbo.Reporter_GetAll
+
+EXEC dbo.Reporter_PutEmail @ReporterId=4, @Email="quigon.jin@readytogo.com"
+
+EXEC dbo.Reporter_PutName @ReporterId=4, @FirstName="Qui Gon", @LastName="Jin"
+
+EXEC dbo.Reporter_PutPasswordHash @ReporterId=4, @PasswordHash="TheForce"
